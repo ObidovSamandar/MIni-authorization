350a4d266d806edc1048f1ef2173c14623e66177
Samandar Obidov <samandarobidovfrd@gmail.com>
2021-02-21 00:09:03 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,40 @@
-let usersList=window.localStorage.getItem('users');
-console.log(window.location)
-if(usersList!=null){
-    usersList=JSON.parse(usersList);
-    for(let elem of usersList){
-        let li=document.createElement('li');
-        
-        for(let key in elem){
-            if(key!='password'){
-                let p=document.createElement('p');
-                p.innerHTML=`<span>${key}</span>:${elem[key]}`;
-                li.appendChild(p);
+let usersList = window.localStorage.getItem('users');
+// console.log(window.location)
+if (usersList != null) {
+    usersList = JSON.parse(usersList);
+    for (let elem of usersList) {
+        let li = document.createElement('li');
+        let divPersonImg = document.createElement('div');
+        divPersonImg.setAttribute('class', 'personImg');
+        let img = document.createElement('img');
+        if (elem['Gender'] == 'Male') {
+            img.setAttribute('src', './groom.png');
+        } else {
+            img.setAttribute('src', './woman.png')
+        }
+        divPersonImg.appendChild(img);
+        li.appendChild(divPersonImg);
+        let divPersonInfo = document.createElement('div');
+        divPersonInfo.setAttribute('class', 'personInfo');
+        for (let key in elem) {
+            if (key != 'password' && key!='Gender') {
+                let p = document.createElement('p');
+                if(key=='FirstName'){
+                    p.innerHTML = `<span>First Name: </span>
+                    ${elem[key]}`;
+                }else if(key=="LastName"){
+                    p.innerHTML = `<span>Last Name: </span>
+                    ${elem[key]}`;
+                }else{
+                    p.innerHTML = `<span>${key}: </span>
+                    ${elem[key]}`;
+                }
+                divPersonInfo.appendChild(p);
             }
         }
-
-        list.appendChild(li);
+        li.appendChild(divPersonInfo);
+        list.appendChild(li)
     }
 }
+
+
